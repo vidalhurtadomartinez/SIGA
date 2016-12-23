4f701ffa8eabf41902772ceec46d91e3e31cbbf6
chevidal <vidal.hurtado@gmail.com>
2016-12-23 14:49:24 -0400

    inicio

---
+++ Helper/ExceptionExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helper
+{
+    public static class ExceptionHelper
+    {
+        public static int LineNumber(this Exception e)
+        {
+            int linenum = 0;
+            try
+            {
+                linenum = Convert.ToInt32(e.StackTrace.Substring(e.StackTrace.LastIndexOf(' ')));
+            }
+
+            catch
+            {
+                //Stack trace is not available!
+            }
+            return linenum;
+        }
+    }
+}
