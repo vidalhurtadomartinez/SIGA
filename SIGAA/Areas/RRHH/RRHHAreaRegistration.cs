b4da75cf2e2d2aacfeef485bb6f154ec70dc1c7e
hurtadomartinezvidal <chevidal@hotmail.com>
2017-01-10 12:43:27 -0400

    modulo CRM insertado

---
+++ SIGAA/Areas/RRHH/RRHHAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace SIGAA.Areas.RRHH
+{
+    public class RRHHAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "RRHH";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "RRHH_default",
+                "RRHH/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "SIGAA.Areas.RRHH.Controllers" }
+            );
+        }
+    }
+}
