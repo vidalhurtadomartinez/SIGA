c510a7fd6c9e8897b10bccaa1f9860f9a1053a3d
ealbarracin <albarracin.edwin@gmail.com>
2017-01-12 14:25:37 -0400

    Update OYM

---
+++ SIGAA/obj/Debug/Package/PackageTmp/Areas/EGRE/Scripts/RecepcionTFGdeTribunal/AgregarDetalleCorrecciones.js
@@ -0,0 +1,22 @@
+﻿$("#btnAgregarObservacion").click(function (eve) {
+    $("#modal-content").load("/EGRE/RecepcionesTFG/CrearDetalleRecepcionTFG");
+});
+
+//definida en el OptionAjax del Ajax Begin Form
+function Success(data) {
+    $("#sObsCorta").val("");
+    $("#sObsDetallada").val("");
+    $("#sSugerencias").val("");
+    $("#sTipoObs_fl").focus();
+    //$("#AgregarObservacionTFG").attr('disabled', true);
+}
+
+function eliminarFila(index) {
+    $("#fila_" + index).remove();
+}
+
+$(".btnEditar").click(function (eve) {
+    $("#modal-content").load("/EGRE/RecepcionesTFG/EliminiarDetalleRecepcionTFG" + $(this).data("id"));
+});
+
+
